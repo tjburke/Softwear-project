5c086c1a11e7f3d53a662fcd7966c287ce12f43e
tjburke <49373656+tjburke@users.noreply.github.com>
2019-04-19 01:28:44 +0100

    Update cards.java

--- cards.java
+++ cards.java
@@ -1,22 +1,26 @@
+/**
+Name: cards
+Description: Makes a deck of cards then shuffles
+Created by tj burke
+*/
 import java.util.Scanner;
 public class    cards {
 
     public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);// creates scanner to get input from user
 
-String[] SUITS = { "Clubs", "Diamonds", "Hearts", "Spades"};
+String[] SUITS = { "Clubs", "Diamonds", "Hearts", "Spades"};// Stores cards
 
- // initialize deck
         int n = SUITS.length * 12;
         String[] deck = new String[n];
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < SUITS.length; j++) {
-                deck[SUITS.length*i + j] = i + " of " + SUITS[j];
+                deck[SUITS.length*i + j] = i + " of " + SUITS[j];// assigns numbers up to 12 for each card
             }
         }
         // shuffle
 		        for (int i = 0; i < n; i++) {
-		            int r = i + (int) (Math.random() * (n-i));
+		            int r = i + (int) (Math.random() * (n-i));// randomises cards
 		            String temp = deck[r];
 		            deck[r] = deck[i];
 		            deck[i] = temp;
