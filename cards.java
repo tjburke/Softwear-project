f492c0fa4e5e84143ec02727a918efdd0982e9f9
tjburke <49373656+tjburke@users.noreply.github.com>
2019-04-29 23:30:04 +0100

    Update cards.java

--- cards.java
+++ cards.java
@@ -4,32 +4,36 @@ Description: Makes a deck of cards then shuffles
 Created by tj burke
 */
 import java.util.Scanner;
+import java.util.Random;
 public class    cards {
 
     public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);// creates scanner to get input from user
-
+		cards c = new cards();
 String[] SUITS = { "Clubs", "Diamonds", "Hearts", "Spades"};// Stores cards
-
-        int n = SUITS.length * 12;
-        String[] deck = new String[n];
+ int n = SUITS.length * 12; // defines n
+        String[] deck = new String[n];// creates new deck with lenght of n
         for (int i = 0; i < 12; i++) {
-            for (int j = 0; j < SUITS.length; j++) {
+
+            for (int j = 0; j < SUITS.length; j++) { // runs for  lenght of suits
                 deck[SUITS.length*i + j] = i + " of " + SUITS[j];// assigns numbers up to 12 for each card
-            }
-        }
-        // shuffle
-		        for (int i = 0; i < n; i++) {
-		            int r = i + (int) (Math.random() * (n-i));// randomises cards
-		            String temp = deck[r];
-		            deck[r] = deck[i];
-		            deck[i] = temp;
-        }
-        // print shuffled deck
-		        for (int i = 0; i < n; i++) {
-		            System.out.println(deck[i]);
-        }
+			}
+		}
+			c.shuffle(deck);// calls shuffle
+			// print shuffled deck
+					         for (int j = 0; j < deck.length; j++) { // runs for  lenght of deck
+		            System.out.println(deck[j]);// prints each cards value
+				}
+
+}
+         public static String[] shuffle(String[] deck){
+					Random rnd = new Random();
+				        for (int i = deck.length - 1;i>= 0;i--) {
+				            int index = rnd.nextInt(i+1);
+				            //swap
+				            String temp = deck[index];
+				            deck[index] = deck[i];
+				            deck[i] = temp;
+		        }
+		        return deck;
 	}
-
-
   }//end class
